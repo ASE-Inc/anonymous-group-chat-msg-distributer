5ebf69eacef355ce5de3f7245cab80f600f095c4
Abhishek Munie <abhishekmunie@yahoo.co.in>
2012-05-28 17:22:30 +0000

    distributed chat system test 1

---
+++ server.js
@@ -0,0 +1,23 @@
+var app = require('http').createServer(function(req, res) {});
+app.listen(process.env.PORT || process.env.C9_PORT || 8001);
+var io = require('socket.io').listen(app),
+    ioClient = require('socket.io-client');
+
+var groups = {},
+    chatServers = ["http://agc1.abhishekmunie.com", "http://agc2.abhishekmunie.com", "http://agc3.abhishekmunie.com"],
+    chatServerSockets = [];
+var chatLen = chatServers.length;
+for (var len=chatLen;len;)(chatServerSockets[len] = new ioClient.Socket(chatServers[--len] + "/")).connect();
+
+var insocket = io.sockets.on('connection', function(socket) {
+    socket.on('createGroup', function(group) {
+        socket.broadcast.emit('createGroup', group);
+        groups[group]={
+            sockets:[]
+        };
+        for (var len=chatLen;len;)(groups[group].sockets[len] = new ioClient.Socket(chatServers[--len] + group)).connect();
+    });
+    socket.on('distributeMessage', function(data) {
+        groups[data.group].sockets[Math.floor(Math.random() * chatLen)].emit("message", data.data);
+    });
+});
