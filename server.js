9ba278c0638bfedc49cee8e23818a5bd6f4c5b1e
Abhishek Munie <abhishekmunie@yahoo.co.in>
2012-05-28 18:38:15 +0000

    Distributed chat network test 2

--- server.js
+++ server.js
@@ -7,17 +7,18 @@ var groups = {},
     chatServers = ["http://agc1.abhishekmunie.com", "http://agc2.abhishekmunie.com", "http://agc3.abhishekmunie.com"],
     chatServerSockets = [];
 var chatLen = chatServers.length;
-for (var len=chatLen;len;)(chatServerSockets[len] = new ioClient.Socket(chatServers[--len] + "/")).connect();
+for (var len = chatLen; len;)(chatServerSockets[len] = new ioClient.Socket(chatServers[--len] + "/")).connect();
 
 var insocket = io.sockets.on('connection', function(socket) {
     socket.on('createGroup', function(group) {
         socket.broadcast.emit('createGroup', group);
-        groups[group]={
-            sockets:[]
-        };
-        for (var len=chatLen;len;)(groups[group].sockets[len] = new ioClient.Socket(chatServers[--len] + group)).connect();
-    });
-    socket.on('distributeMessage', function(data) {
-        groups[data.group].sockets[Math.floor(Math.random() * chatLen)].emit("message", data.data);
+        //groups[group] = {sockets: []};
+        io.of(group).on('connection', function(socket) {
+            socket.on('message', function(data) {
+                socket.broadcast.emit("message", data);
+            });
+        });
+        //for (var len=chatLen;len;)(groups[group].sockets[len] = new ioClient.Socket(chatServers[--len] + group)).connect();
+        //groups[data.group].sockets[Math.floor(Math.random() * chatLen)].emit("message", data.data);
     });
 });
